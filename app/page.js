1b177244b397c699848a99e1f577ef92956f7ee9
AdamC7313 <AdamC7313@gmail.com>
2024-03-12 08:53:59 -0400

    add analytics

--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ import About from "./components/about/about";
 import Projects from "./components/projects/projects";
 import Skills from "./components/skills/skills";
 import Contact from "./components/contact/contact";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <main>
+      <Analytics />
       <Nav />
       <Head />
       <About />
