ec9bf2c862aff459f0b0af3e426332b2de8ec111
AdamC7313 <AdamC7313@gmail.com>
2024-08-27 19:57:49 -0400

    build nav bar

--- app/components/head/head.js
+++ app/components/head/head.js
@@ -4,10 +4,11 @@ import "./head.css";
 const Head = () => {
   return (
     <div className="head-container">
-      <h1 className="title" style={{textAlign: "center"}}>Hi, I&apos;m Adam.</h1>
-      <h2 className="subtitle">
+      <h1 className="title">Adam Cruz</h1>
+      <h2 className="subtitle">Software Engineer</h2>
+      <h4 className="me-description">
         I design and build software that&apos;s beautiful fast, scalable, and user-focused.
-      </h2>
+      </h4>
     </div>
   );
 };
--- app/components/nav/nav.js
+++ app/components/nav/nav.js
@@ -4,65 +4,69 @@ import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 import "./nav.css";
 
 const Nav = () => {
-  const [isScrolling, setIsScrolling] = useState(false);
+  const [isScrolling, setIsScrolling] = useState("");
 
   useEffect(() => {
     const handleScroll = () => {
-      // Check if the user has scrolled past a certain point (e.g., 100px)
-      const scrolled = window.scrollY > 100;
-      setIsScrolling(scrolled);
+      const scrollPosition = window.scrollY;
+      console.log(scrollPosition);
+      if (scrollPosition < 900) {
+        setIsScrolling("about");
+      } else if (scrollPosition >= 900 && scrollPosition < 3800) {
+        setIsScrolling("projects");
+      } else if (scrollPosition >= 3800 && scrollPosition < 4570) {
+        setIsScrolling("skills");
+      } else if (scrollPosition >= 4570) {
+        setIsScrolling("contact");
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
-
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
     <>
-    <div className={isScrolling ? "nav floating" : "nav"}>
-      <div className="pages">
-        <ScrollLink
-          to="about"
-          smooth={true}
-          duration={500}
-          className="menu-item"
-          offset={-80}
-        >
-          about
-        </ScrollLink>
-        <ScrollLink
-          to="projects"
-          smooth={true}
-          duration={500}
-          className="menu-item"
-          offset={-79}
-        >
-          projects
-        </ScrollLink>
-        <ScrollLink
-          to="skills"
-          smooth={true}
-          duration={500}
-          className="menu-item"
-          offset={-81}
-        >
-          skills
-        </ScrollLink>
-        <ScrollLink
-          to="contact"
-          smooth={true}
-          duration={500}
-          className="menu-item last"
-          offset={-80}
-        >
-          contact
-        </ScrollLink>
+      <div className="nav">
+        <div className="pages">
+          <ScrollLink
+            to="about"
+            smooth={true}
+            duration={500}
+            className={isScrolling === "about" ? "active-menu-item" : "menu-item"}
+          >
+            about
+          </ScrollLink>
+          <ScrollLink
+            to="projects"
+            smooth={true}
+            duration={500}
+            className={isScrolling === "projects" ? "active-menu-item" : "menu-item"}
+          >
+            projects
+          </ScrollLink>
+          <ScrollLink
+            to="skills"
+            smooth={true}
+            duration={500}
+            className={isScrolling === "skills" ? "active-menu-item" : "menu-item"}
+          >
+            skills
+          </ScrollLink>
+          <ScrollLink
+            to="contact"
+            smooth={true}
+            duration={500}
+            className={isScrolling === "contact" ? "active-menu-item" : "menu-item last"}
+          >
+            contact
+          </ScrollLink>
+        </div>
       </div>
-    </div>
-    <div className={`scroll-for-more ${isScrolling ? 'scrolled' : ''}`}>
+      <div className={`scroll-for-more ${isScrolling ? "scrolled" : ""}`}>
         scroll for more
       </div>
     </>
