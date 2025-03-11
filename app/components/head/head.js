6c38fd035d47622e149a7f81a2967d383432a78e
Adam Cruz <113055940+AdamC7313@users.noreply.github.com>
2025-03-11 15:30:50 -0400

    Add comma

--- app/components/head/head.js
+++ app/components/head/head.js
@@ -7,7 +7,7 @@ const Head = () => {
       <h1 className="title">Adam Cruz</h1>
       <h2 className="subtitle">Software Engineer</h2>
       <h4 className="me-description">
-        I design and build software that&apos;s beautiful fast, scalable, and user-focused.
+        I design and build software that&apos;s beautiful, fast, scalable, and user-focused.
       </h4>
     </div>
   );
